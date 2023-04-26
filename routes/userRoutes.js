0f98de7e8fb87f3c1a69071241d50e7affee41fd
Vikrant Borawake <vikrantswaron@gmail.com>
2023-04-26 22:27:37 +0530

    Signup route added

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -6,6 +6,7 @@ const authController = require('../controllers/authController');
 
 router.route('/').get(userController.getAll).post(userController.addOne);
 router.post('/login', authController.login);
+router.post('/signup', authController.signUp);
 router.post('/verify', userController.verify);
 router.post('/forgotPassword', authController.forgotPassword);
 router.patch('/resetPassword/:token', authController.resetPassword);
